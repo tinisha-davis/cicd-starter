e0fcc50a36383d883424013135d63f74e112749e
Tinisha <tinishadavis159@gmail.com>
2026-01-20 19:50:35 -0800

    Breaking the code on purpose to show a workflow fail

--- src/math_utils.py
+++ src/math_utils.py
@@ -3,4 +3,5 @@ def add(a: int, b: int) -> int:
     Return the sum of two integers.
     Students will deliberately break this, then fix it to make CI pass.
     """
-    return a + b
+    return a / b
+
